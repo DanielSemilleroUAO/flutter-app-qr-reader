46b216664801c7fc5921dc9e3c1e20d8ac56dced
Daniel Delgado Rodríguez <danieldelgado_2106@hotmail.com>
2022-05-13 19:46:25 -0500

    Upload project

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/models/scan_model.dart';
+import 'package:qr_reader/pages/direcciones_page.dart';
+import 'package:qr_reader/pages/mapas_page.dart';
+import 'package:qr_reader/providers/db_provider.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
+import 'package:qr_reader/widgets/custom_navigatiorbar.dart';
+import 'package:qr_reader/widgets/scan_button.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Historial'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.delete_forever))
+        ],
+      ),
+      body: _HomePageBody(),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  const _HomePageBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final uiProvider = Provider.of<UiProvider>(context);
+    final currenteIndex = uiProvider.selectedMenuOpt;
+    final tempScan = new ScanModel(id: 1, tipo: 'http', valor: 'http');
+    DBProvider.db.nuevoScan(tempScan);
+    switch (currenteIndex) {
+      case 0:
+        return MapasPage();
+      case 1:
+        return DireccionesPage();
+      default:
+        return MapasPage();
+    }
+  }
+}
---
+++ lib/providers/db_provider.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:qr_reader/models/scan_model.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DBProvider {
+  static Database? _database;
+  static final DBProvider db = DBProvider._();
+  DBProvider._();
+
+  get database async {
+    if (_database != null) {
+      return _database;
+    }
+    _database = await initDB();
+    return _database;
+  }
+
+  Future<Database> initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'ScansDB.db');
+    print(path);
+    return await openDatabase(path, version: 1, onOpen: (db) {},
+        onCreate: (Database db, int version) async {
+      await db.execute('''
+            CREATE TABLE Scans(
+              id INTEGER PRIMARY KEY,
+              tipo TEXT,
+              valor TEXT
+            );
+          ''');
+    });
+  }
+
+  nuevoScanRaw(ScanModel nuevoScan) async {
+    final id = nuevoScan.id;
+    final tipo = nuevoScan.tipo;
+    final valor = nuevoScan.valor;
+    final db = await database;
+    final res = await db.rawInsert('''
+      INSERT INTO Scans(id,tipo,valor)
+        VALUES($id,'$tipo','$valor')
+    ''');
+    return res;
+  }
+
+  nuevoScan(ScanModel nuevoScan) async {
+    final db = await database;
+    final res = await db.insert('Scans', nuevoScan.toJson());
+    print(res);
+    return res;
+  }
+}
